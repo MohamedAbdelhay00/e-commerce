58845f983692329cd359011bad9338d30c798c87
MohamedAbdelhay00 <191800016@ecu.edu.eg>
2024-03-02 15:35:56 +0200

    final commit

--- src/Modules/Authentication/Components/Register/Register.jsx
+++ src/Modules/Authentication/Components/Register/Register.jsx
@@ -24,7 +24,7 @@ export default function Register() {
       }, 500);
     } catch (error) {
       console.log(error);
-      toast.error(error.response.data.errors.msg, {
+      toast.error(error.response.data.message, {
         position: "bottom-left"
       });
     }
